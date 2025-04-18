e43c13a3cc6451d4a848f28517df8cebd2f14151
RadiTEFA <radingoding@gmail.com>
2025-04-18 18:24:19 +0700

    Add files via upload

--- src/components/UI/RedButton.vue
+++ src/components/UI/RedButton.vue
@@ -1,3 +1,3 @@
 <template>
-    <buton class="bg-red-600 rounded px-2 py-1 text-white hover:bg-red-300"><slot></slot></buton>
+    <button class="bg-red-600 rounded px-2 py-1 text-white hover:bg-red-300"><slot></slot></button>
 </template>
